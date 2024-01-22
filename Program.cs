80850269c080818fb53769d7f57541f3285a28bd
ArmanSahota <109508805+ArmanSahota@users.noreply.github.com>
2024-01-21 19:32:24 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        PrintNumbersFrom1To10(1);
+    }
+    static void PrintNumbersFrom1To10(int currentNumber)
+    {
+        if (currentNumber <= 10)
+        {
+            Console.WriteLine(currentNumber);
+
+            int nextNumber = currentNumber + 1;
+            PrintNumbersFrom1To10(nextNumber);
+        }
+    }
+}
